e095693457bb2b14f56ff419f62bdbfa15deb734
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-04-09 20:00:52 +0300

    add objects fro price factors

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,20 +1,28 @@
 let price = 0;
 const carForm = document.forms.carForm;
+
 const makeInput = carForm.elements.make;
 const makeError = document.getElementById("makeError");
+
 const modelInput = carForm.elements.model;
 const modelError = document.getElementById("modelError");
+
 const fuelInput = carForm.elements.fuel;
 const fuelError = document.getElementById("fuelError");
+
 const volumeInput = carForm.elements.engineVolume;
 const volumeError = document.getElementById("engineVolumeError");
+
 const transmissionInput = carForm.elements.transmission;
 const transmissionError = document.getElementById("transmissionError");
+
 const conditionInput = carForm.elements.carCondition;
 const conditionError = document.getElementById("conditionError");
+
 const ownersField = document.getElementById("ownersField");
 const ownersInput = carForm.elements.numOfOwners;
 const ownersError = document.getElementById("ownersError");
+
 const paymentInput = carForm.elements.payment;
 const paymentError = document.getElementById("paymentError");
 
@@ -23,6 +31,51 @@ const models = {
   Opel: ["Opel Astra", "Opel Insignia", "Opel Mokka"],
   Mazda: ["Mazda 3", "Mazda CX-5", "Mazda 6"],
   Jaguar: ["Jaguar F-type", "Jaguar XE", "Jaguar F-Pace"],
+};
+
+const basePrices = {
+  "Renault Duster": 1200000,
+  "Renault Megane": 1400000,
+  "Renault Arkana": 1450000,
+  "Opel Astra": 1000000,
+  "Opel Insignia": 1400000,
+  "Opel Mokka": 1300000,
+  "Mazda 3": 1300000,
+  "Mazda CX-5": 2000000,
+  "Mazda 6": 1600000,
+  "Jaguar F-type": 5000000,
+  "Jaguar XE": 2500000,
+  "Jaguar F-Pace": 3500000,
+};
+
+const fuelPriceFactor = {
+  petrol: 1.0,
+  dieasel: 1.05,
+  gas: 0.95,
+  electricity: 1.2,
+};
+
+const engVolPriceFactor = {
+  1.6: 1.0,
+  "2.0": 1.1,
+  2.5: 1.2,
+  3.5: 1.3,
+};
+
+const transmissionPriceFactor = {
+  manual: 1.0,
+  automatic: 50000,
+  cvt: 30000,
+};
+
+const conditionPriceFactor = {
+  new: 1.0,
+  used: 0.75,
+};
+
+const numOfOwnersPriceFactor = {
+  "1-2": 1.0,
+  "3+": 0.9,
 };
 
 const formValidity = {
@@ -44,7 +97,7 @@ const errorMsgs = {
     "Объем двигателя не может быть меньше 1.1 литра и больше 3.5 литра",
   noTransmissionErrorMsg: "Пожалуйста, выберите тип коробки передач",
   noConditionErrorMsg: "Пожалуйста, выберите состояние автомобиля",
-  noOwnersErrorMsg: "Пожалуйстаб выберите количество предыдущих владельцев",
+  noOwnersErrorMsg: "Пожалуйста, выберите количество предыдущих владельцев",
   noPaymentErrorMsg: "Пожалуйста, выберите способ оплаты",
 };
 
@@ -129,10 +182,13 @@ function toggleOwnersState(conditionValue) {
 }
 
 function processRadioOrSelectChange(errorElem, name) {
-  if (name === "make") {
-    showModels(makeInput.value);
-  } else if (name === "carCondition") {
-    toggleOwnersState(conditionInput.value);
+  switch (name) {
+    case "make":
+      showModels(makeInput.value);
+      break;
+
+    case "carCondition":
+      toggleOwnersState(conditionInput.value);
   }
 
   toggleErrorMsg(true, errorElem, "");
@@ -170,4 +226,11 @@ function processFormChange(evt) {
   }
 }
 
+function submitForm(evt) {
+  evt.preventDefault();
+  // check if all inputs are valid
+  regForm.reset();
+}
+
 carForm.addEventListener("change", processFormChange);
+carForm.addEventListener("submit", submitForm);
